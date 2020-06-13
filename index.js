17615e4f6941523aa8576955e7cf7ef00f2d415d
Andrew Shumanskiy <andrey.shumanskiy@gmail.com>
2020-06-13 15:57:04 +0300

    rename show/hide output

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let open = false;
 $('#showHideContent').click(function() {
-    $("#showHideContent").html(open = !open? 'Свернуть': 'LANL Eartquake Prediction');
+    $("#showHideContent").html(open = !open? 'LANL Eartquake Prediction': 'LANL Eartquake Prediction');
 });
 $(document).ready(function(){
     $("#showHideContent").click(function () {
